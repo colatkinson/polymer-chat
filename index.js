b8f716141ad14d46f4f254ebd0c67f9a6f43e311
Colin Atkinson <colin.william.atkinson@gmail.com>
2014-12-20 12:21:00 -0500

    Add some heroku support stuff

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ var b = moment().utc(a);
 console.log(a.format("h:m A [on] MMM Do"));
 console.log(b.format("h:m A [on] MMM Do"));*/
 
+var port = (process.env.PORT || 3000);
+
 var router = express.Router();
 
 router.get('/', function(req, res){
@@ -24,27 +26,29 @@ names = {};
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  console.log(socket.id);
+  //console.log(Object.keys(names).length + " online");
 
   socket.on("set name", function(name) {
     console.log("Name: " + name);
     names[socket.id] = name;
+    console.log(Object.keys(names).length + " online");
     io.emit('connections', Object.keys(names).map(function (key) {return names[key]}));
     socket.on('chat message', function(msg){
-        console.log('message: ' + msg);
+        //console.log('message: ' + msg);
         io.emit('chat message', {from: name, txt: msg, time: moment.utc()});
-        console.log(moment.utc());
+        //console.log(moment.utc());
     });
   });
   socket.on('disconnect', function(){
     console.log('user disconnected');
     delete names[socket.id];
+    console.log(Object.keys(names).length + " online");
     io.emit('connections', Object.keys(names).map(function (key) {return names[key]}));
   });
 });
 
 app.use('/', router);
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:'+port);
 });
